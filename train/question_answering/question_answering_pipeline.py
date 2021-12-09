aee51a3cb67e21e92c382d217e8e5673977a50ec
gabinguo <gabin.guo@gmail.com>
2021-12-09 16:58:10 +0100

    implement the part to fine-tune the lm

--- train/question_answering/question_answering_pipeline.py
+++ train/question_answering/question_answering_pipeline.py
@@ -1,11 +1,15 @@
 import argparse
+import json
 import logging
+import os.path
 
-from preparation.dataset_processing import prepare_train_features, prepare_validation_features
+from preparation.dataset_processing import prepare_train_features, prepare_validation_features, \
+    postprocess_qa_predictions
+from preparation.dataset_curation import convert_jsonlines_to_dataset
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 from transformers import TrainingArguments, Trainer, IntervalStrategy
 from transformers import default_data_collator
-from datasets import Dataset, DatasetDict
+from datasets import Dataset, DatasetDict, load_metric
 from utils.log import setup_logger, log_map
 import torch
 
@@ -70,16 +74,26 @@ def train_model(params):
     # evaluation
     raw_predictions = trainer.predict(test_features)
     test_features.set_format(type=test_features.format["type"], columns=list(test_features.features.keys()))
+    final_predictions = postprocess_qa_predictions(tokenizer, dataset_dict["test"], test_features,
+                                                   raw_predictions.predictions,
+                                                   max_answer_length=params.max_answer_length)
+    formatted_predictions = [{"id": k, "prediction_text": v} for k, v in final_predictions.items()]
+    references = [{"id": ex["id"], "answers": ex["answers"]} for ex in dataset_dict["test"]]
+    result = metric.compute(predictions=formatted_predictions, references=references)
+    # write result to file for records
+    with open(os.path.join(output_dir, "eval_result.json"), 'w') as f:
+        json.dump(result, f)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train arguments parser")
-    parser.add_argument("--train_csv", type=str, help="Filename to the train .csv file")
-    parser.add_argument("--test_csv", type=str, help="Filename to the test .csv file")
+    parser.add_argument("--train_file", type=str, help="Filename to the train .json file")
+    parser.add_argument("--test_file", type=str, help="Filename to the test .json file")
     parser.add_argument("--model_ckpt", type=str, help="Path to the model folder.")
     parser.add_argument("--output_dir", type=str, help="Path to put the trained model.")
     parser.add_argument("--doc_stride", type=int, default=128, help="Overlap between chunks.")
     parser.add_argument("--max_seq_len", type=int, default=384, help="Length limit for chunks.")
+    parser.add_argument("--max_answer_length", type=int, default=48, help="Max length of the answers.")
     parser.add_argument("--batch_size", type=int, default=16, help="Batch size of training.")
     parser.add_argument("--num_epochs", type=int, default=2, help="Number of training epochs.")
     parser.add_argument("--lr", type=float, default=2e-5, help="Learning rate")
@@ -87,9 +101,21 @@ if __name__ == '__main__':
     parser.add_argument("--use_gpu", type=bool, default=True, help="Flag of gpu / cpu")
 
     args = parser.parse_args()
+    try:
+        train_data = Dataset.from_json(args.train_file)
+        test_data = Dataset.from_json(args.test_file)
+    except Exception as e:
+        logger.warning("Exception occurs when try to read .json using Dataset.from_json")
+        logger.warning(e)
+        train_data = convert_jsonlines_to_dataset(args.train_file)
+        test_data = convert_jsonlines_to_dataset(args.test_file)
+
     dataset_dict = DatasetDict({
-        "train": Dataset.from_csv(args.train_csv),
-        "test": Dataset.from_csv(args.test_csv)
+        "train": train_data,
+        "test": test_data
     })
+
+    # use squad metric for evaluating the EM and F1
+    metric = load_metric("squad")
 
     train_model(args)
